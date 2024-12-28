55d89278d93b1fd58bffc155c8585639c8425e00
ayush kumar <ayushkumar09005@gmail.com>
2024-12-28 20:27:33 +0530

    first commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -71,7 +71,8 @@ const Home = () => {
               <div className="arc"></div>
               <div className="text">
                 <p>ranchi</p>
-                <h2>Service in Ranchi</h2>
+                {isWideScreen ? <h2>Service in Ranchi</h2>: <h2>Services</h2>}
+              
               </div>
             </div>
           </div>
